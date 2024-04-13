debdc0141356c5b9a3cdcda9cf8dfc5dfbe753d4
19Aleksandra95 <polar.heder@gmail.com>
2024-04-13 21:39:11 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWithRequest  from 'components/AppWithRequest';
+import {App} from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<AppWithRequest />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
